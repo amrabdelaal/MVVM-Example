3ed6eab3f2c71b7333e21aa5d00c27b106b5a1a6
Amr Abdelaal <amr.abdelaal@denkwerk.com>
2020-02-25 14:59:57 +0100

    Initial Commit

---
+++ MVVM-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-Example
+//
+//  Created by Amr Abdelaal on 25.02.20.
+//  Copyright © 2020 Amr Abdelaal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
