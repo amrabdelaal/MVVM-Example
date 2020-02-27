dd1d9098f1511d3943fd4deb49f36d9fdc931f6f
Amr Abdelaal <amr.abdelaal@denkwerk.com>
2020-02-27 13:51:37 +0100

    -- Add Skelton Files

---
+++ MVVM-Example/ViewModels/ReposViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ReposViewModel.swift
+//  MVVM-Example
+//
+//  Created by Amr Abdelaal on 27.02.20.
+//  Copyright © 2020 Amr Abdelaal. All rights reserved.
+//
+
+import Foundation
--- MVVM-Example/Views/ReposViewController.swift
+++ MVVM-Example/Views/ReposViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ReposViewController.swift
 //  MVVM-Example
 //
 //  Created by Amr Abdelaal on 25.02.20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ReposViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
