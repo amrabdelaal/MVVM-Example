9395f85bae5718a511a53a1ea3a395c4fe9dacf4
Amr Abdelaal <amrabdelaalmo@gmail.com>
2023-06-27 00:19:32 +0200

    Update Dynamic.swift

--- MVVM-Example/ViewModels/Dynamic.swift
+++ MVVM-Example/ViewModels/Dynamic.swift
@@ -3,20 +3,18 @@
 //  MVVM-Example
 //
 //  Created by Amr Abdelaal on 27.02.20.
-//  Copyright © 2020 Amr Abdelaal. All rights reserved.
-//
 
 import Foundation
 
-class Dynamic<T> {
+struct Dynamic<T> {
     typealias Listener = (T) -> ()
     var listener: Listener?
     
-    func bind(_ listener: Listener?) {
+    mutating func bind(_ listener: Listener?) {
         self.listener = listener
     }
     
-    func bindAndFire(_ listener: Listener?) {
+    mutating func bindAndFire(_ listener: Listener?) {
         self.listener = listener
         listener?(value)
     }
